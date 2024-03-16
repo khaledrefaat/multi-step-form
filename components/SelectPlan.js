6ca9060154cf2c96f468f54b47b6be23718a2430
Khaled Refaat Elkady <khaledrefaat08@gmail.com>
2024-03-16 05:55:12 +0200

    finished the style and javascript for the select-plan step (desktop version)

--- components/SelectPlan.js
+++ components/SelectPlan.js
@@ -1,5 +1,5 @@
 import CssLoader from '../services/CssLoader.js';
-import { $ } from '../utils/helperFunctions.js';
+import { $, $$ } from '../utils/helperFunctions.js';
 
 export class SelectPlan extends HTMLElement {
   constructor() {
@@ -7,6 +7,36 @@ export class SelectPlan extends HTMLElement {
     this.root = this.attachShadow({ mode: 'open' });
     this.cssLoader = new CssLoader(this.root);
     this.cssLoader.loadCss('/components/SelectPlan.css');
+  }
+
+  toggleAnnualPlan() {
+    const checkBox = $('input[type="checkbox"]', this.root);
+
+    checkBox.addEventListener('click', () => {
+      $('.monthly', this.root).classList.toggle('active', !checkBox.checked);
+      $('.yearly', this.root).classList.toggle('active', checkBox.checked);
+
+      // switch between yearly and monthly price
+      $$('.plan', this.root).forEach(plan => {
+        $$('p', plan).forEach(plan => {
+          const priceText = plan.textContent;
+          const price = priceText.replace(/\D/g, '');
+
+          const newPrice = checkBox.checked ? price * 10 : price / 10;
+          const newPriceText = `$${newPrice}/${checkBox.checked ? 'yr' : 'mo'}`;
+        });
+
+        $('span', plan).style.display = checkBox.checked
+          ? 'inline-block'
+          : 'none';
+      });
+    });
+  }
+
+  togglePlan() {
+    $$('.plan', this.root).forEach(plan => {
+      plan.classList.remove('active-plan');
+    });
   }
 
   connectedCallback() {
@@ -19,6 +49,15 @@ export class SelectPlan extends HTMLElement {
       'You have the option of monthly or yearly billing.';
     $('[slot="back-btn"]', this.root).href = '/';
     $('[slot="next-btn"]', this.root).href = '/add-ons';
+
+    this.toggleAnnualPlan();
+
+    $$('.plan', this.root).forEach(plan => {
+      plan.addEventListener('click', () => {
+        this.togglePlan();
+        plan.classList.add('active-plan');
+      });
+    });
   }
 }
 
